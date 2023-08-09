6b2007583089eea9c5d139100d5e1d662834b490
Soubhagya-280199 <ssethy.srs@gmail.com>
2023-08-09 10:59:35 +0530

    postgres connection establishment

--- test.py
+++ test.py
@@ -1,12 +1,59 @@
 import psycopg2
+import os
+import pandas as pd
 
-try:
-    conn = psycopg2.connect(host='localhost', dbname='postgres', user='postgres', password='Bantypg', port=5432)
-    print("Success")
+def conection(user_name,pwd,host_name):
+    try:
+        conn = psycopg2.connect(host=host_name, dbname='new_db', user=user_name, password=pwd, port=5432)
 
-    cur = conn.cursor()
+        print("successfully connected")
+        cur = conn.cursor()
 
-except Exception as er:
-    print(er)
+        return cur
 
-#janmejay as curse
+    except Exception as er:
+        print(er)
+
+def get_col_headers(cur):
+    try:
+        print("col headers called")
+        cur.execute("SELECT * FROM  public.stud_dtl LIMIT 0 ;")
+        headers = cur.fetchall()
+        print(headers)
+        return headers
+
+    except Exception as er:
+        print(er)
+def get_sql_data(cur):
+    try:
+        cur.execute("SELECT * FROM public.stud_dtl;")
+        data = cur.fetchall()
+        print(data)
+        return data
+
+    except Exception as er:
+        print(er)
+
+def do_start():
+    user_name = os.environ.get('db_username')
+    pwd = os.environ.get('db_password')
+    host_name = os.environ.get('db_host')
+
+
+    #step: 1
+    cur = conection(user_name,pwd,host_name)
+    #step: 2
+    headers = get_col_headers(cur)
+    print(headers)
+
+    #step: 3
+    data = get_sql_data(cur)
+    data_df = pd.DataFrame(data,columns=headers)
+
+    print(data_df)
+
+
+if __name__ == '__main__':
+    do_start()
+
+
